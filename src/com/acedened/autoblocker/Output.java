5cb080a114383b576f411775ebf968e90e4af689
danpetruk <d_petruk@mail.ru>
2015-01-08 20:42:54 +0400

    разделил интерфейс и логику

---
+++ src/com/acedened/autoblocker/Output.java
@@ -0,0 +1,19 @@
+package com.acedened.autoblocker;
+
+public abstract class Output {
+
+    abstract void connectedToGroup();
+
+    abstract void error(Exception e);
+
+    abstract void foundAppleId(String id);
+
+    abstract void startedBlockingAppleId(String id);
+
+    abstract void attemptBlockingAppleId(String id, String password);
+
+    abstract void endedBlockingAppleId(String id);
+
+    abstract void ended(String listOfIds[]);
+
+}
